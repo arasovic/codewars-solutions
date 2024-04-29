51ebfe9c9104173818daf3b397a0d406e8110c55
arasovic <arasmehmet07@gmail.com>
2024-04-30 01:00:57 +0300

    feat: array diff on time comp

--- array-diff/index.js
+++ array-diff/index.js
@@ -8,5 +8,11 @@ function arrayDiff1(a, b) {
   return a.filter((el) => b.indexOf(el) === -1);
 }
 
+// solution 3 with O(n) time complexity
+function arrayDiff2(a, b) {
+  const bSet = new Set(b);
+  return a.filter((el) => !bSet.has(el));
+}
+
 console.log(arrayDiff([1, 2], [1])); // [1,3]
 console.log(arrayDiff([1, 2, 2, 2, 3], [2])); // [1,3]
